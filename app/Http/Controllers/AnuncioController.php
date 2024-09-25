f0295a21821e78f8405380247ad815bc8344c8d8
Brandon Rodrigues <bamerico968@gmail.com>
2024-09-24 21:49:39 -0300

    CommitTradeFromCategoriaToAnuncio

---
+++ app/Http/Controllers/AnuncioController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Categoria;
+
+//TROCAR TUDO P/ ANUNCIO -> 17:52 EM CRUD POSTAGEM INDEX CREATE SHOW.
+
+class CategoriaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $categorias = Categoria::orderBy('id', 'ASC')->get();
+        
+       //dd($categorias);
+
+        return view('categoria.categoria_index', compact('categorias'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        // dd('OLÁ');
+        return view ('categoria.categoria_create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        // dd($request->all());,
+
+        $validated = $request->validate([
+            'nome' => 'required|min:5',
+
+        ]);
+
+        $categoria = new Categoria();
+        $categoria->nome = $request->nome;
+        $categoria-> save();
+
+        return redirect()->route('categoria.index')->with('mensagem', 'Categoria cadastrada com sucesso!');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        // dd('show:'. $id);
+        $categoria = Categoria::find($id);
+        return view('categoria.categoria_show', compact('categoria'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $categoria = Categoria::find($id);
+        return view('categoria.categoria_edit', compact('categoria'));
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        
+        
+        
+        $validated = $request->validate([
+            'nome' => 'required|min:5',
+
+        ]);
+
+        $categoria = Categoria::find($id);
+        $categoria->nome =  $request->nome;
+        $categoria->save();
+
+        return redirect()->route('categoria.index')->with('mensagem', 'Categoria alterada com sucesso!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $categoria = Categoria::find($id);
+        $categoria->delete();
+
+        return redirect()->route('categoria.index')->with('mensagem', 'Categoria excluida com sucesso!');
+    }
+}
