5cdfce0442b8977e552db6aee88db2ff5899b892
César Mousinho <cesar.mousinho.147@gmail.com>
2024-09-26 21:25:20 -0300

    CommitRevisãoAndré

--- resources/views/categoria/categoria_create.blade.php
+++ resources/views/categoria/categoria_create.blade.php
@@ -1,6 +1,12 @@
 @extends('adminlte::page')
 
 @section('content')
+
+<!-- -------- ABERTURA DO EDITOR RICO ----------- -->
+<link rel="stylesheet" href="{{url('/richtexteditor/rte_theme_default.css')}}" />
+<script type="text/javascript" src="{{url('/richtexteditor/rte.js')}"></script>
+<script type="text/javascript" src="{{url('/richtexteditor/plugins/all_plugins.js') }}"></script>
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -33,6 +39,18 @@
                         
                         <input type="submit" value="ENVIAR">
                     </form>
+
+                    <!--  FIM DO FORM  -->
+
+                    <!-- QUANDO FOR ADD O ANUNCIO_SHOW_BLADE.PHP LEMBRE DE COLOCAR <p><strong> Conteúdo</strong> {!! $postagem->conteudo !!} </p>
+                        E NO ID DENTRO DO FORM (AONDE ESTÁ O NOSSO QUERIDO CONTEUDO) TROQUE PARA : "inp_editor1"  IGUAL O SCRIPT QUE FECHA O EDITOR RICO!
+                    -->
+
+
+                    <!-- -------- FECHAMENTO DO EDITOR RICO------- -->
+                    <script>
+                        var editor1 = new RichTextEditor("#inp_editor1");
+                    </script>
 
             </div>   
         </div>
