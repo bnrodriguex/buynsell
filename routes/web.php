53c59de165b00ccd2ee2ecb0dbf43f275a387e68
Brandon Rodrigues <bamerico968@gmail.com>
2024-09-26 21:43:59 -0300

    COMMITFINAL

--- routes/web.php
+++ routes/web.php
@@ -44,3 +44,30 @@ Route::middleware(['auth'])->group(function () {
 
     Route::get('/categoria/{id}', [CategoriaController::class, 'show'])->name('categoria.show');
 
+    Route::put('/categoria/{id}', [CategoriaController::class, 'update'])->name('categoria.update');
+
+    Route::delete('/categoria/{id}',[CategoriaController::class, 'destroy'])->name('categoria.destroy');
+
+
+
+// --------- ANÚNCIOS --------------
+
+
+    Route::get('/anuncio', [AnuncioController::class, 'index'])->name('anuncio.index');
+
+    Route::get('/anuncio/create', [AnuncioController::class, 'create'])->name('anuncio.create');
+
+    Route::post('/anuncio', [AnuncioController::class, 'store'])->name('anuncio.store');
+
+    Route::get('/anuncio/{id}/edit', [AnuncioController::class, 'edit'])->name('anuncio.edit');
+
+    Route::get('/anuncio/{id}', [AnuncioController::class, 'show'])->name('anuncio.show');
+
+    Route::put('/anuncio/{id}', [AnuncioController::class, 'update'])->name('anuncio.update');
+
+    Route::delete('/anuncio/{id}', [AnuncioController::class, 'destroy'])->name('anuncio.destroy');
+
+});
+
+
+
